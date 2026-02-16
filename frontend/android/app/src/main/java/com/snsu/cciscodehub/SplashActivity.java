ea9fe99ba5e26cacb930166dc1cf9c6451242260
mrfost07 <mfostanes@ssct.edu.ph>
2026-02-16 16:51:54 +0800

    fix: final splash screen revert to VideoView with re-encoded codec

--- frontend/android/app/src/main/java/com/snsu/cciscodehub/SplashActivity.java
+++ frontend/android/app/src/main/java/com/snsu/cciscodehub/SplashActivity.java
@@ -2,25 +2,21 @@ package com.snsu.cciscodehub;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Surface;
-import android.view.TextureView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.VideoView;
 
-public class SplashActivity extends Activity implements TextureView.SurfaceTextureListener {
-
-    private MediaPlayer mediaPlayer;
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen before setting content
+        // Fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -29,69 +25,38 @@ public class SplashActivity extends Activity implements TextureView.SurfaceTextu
 
         setContentView(R.layout.activity_splash);
 
-        TextureView textureView = findViewById(R.id.splashVideo);
-        textureView.setSurfaceTextureListener(this);
-    }
+        VideoView videoView = findViewById(R.id.splashVideo);
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash_video);
+        
+        videoView.setVideoURI(videoUri);
+        
+        videoView.setOnPreparedListener(mp -> {
+            // Ensure video scaling is set correctly
+            mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+            videoView.start();
+        });
 
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
-        try {
-            mediaPlayer = new MediaPlayer();
-            Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash_video);
-            mediaPlayer.setDataSource(this, videoUri);
-            mediaPlayer.setSurface(new Surface(surfaceTexture));
-            mediaPlayer.setOnPreparedListener(mp -> {
-                mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-                mp.start();
-            });
-            mediaPlayer.setOnCompletionListener(mp -> startMainActivity());
-            mediaPlayer.setOnErrorListener((mp, what, extra) -> {
-                startMainActivity();
-                return true;
-            });
-            mediaPlayer.prepareAsync();
-        } catch (Exception e) {
+        videoView.setOnCompletionListener(mp -> startMainActivity());
+        
+        videoView.setOnErrorListener((mp, what, extra) -> {
+            // Fail gracefully - just go to login if video fails
             startMainActivity();
-        }
-    }
-
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {}
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        releasePlayer();
-        return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {}
-
-    private void releasePlayer() {
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
+            return true;
+        });
     }
 
     private void startMainActivity() {
-        releasePlayer();
         Intent intent = new Intent(this, MainActivity.class);
         if (getIntent() != null && getIntent().getData() != null) {
             intent.setData(getIntent().getData());
         }
         startActivity(intent);
         finish();
+        
         if (Build.VERSION.SDK_INT >= 34) {
             overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0);
         } else {
             overridePendingTransition(0, 0);
         }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        releasePlayer();
     }
 }
